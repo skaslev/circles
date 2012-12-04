a8ec77d34f94a4e71e35eaad860f79f6d6315435
Slavomir Kaslev <slavomir.kaslev@gmail.com>
2012-12-04 01:01:38 -0800

    Removed some copy-pasted code.

--- circles.js
+++ circles.js
@@ -17,9 +17,8 @@ var vec = {
   },
 
   clone: function(a) {
-    var b = [];
-    for (var i = 0; i < vec.DIM; i++)
-      b.push(a[i]);
+    var b = vec.zero();
+    vec.copy(b, a);
     return b;
   },
 
@@ -89,10 +88,6 @@ var circ = {
   }
 };
 
-function rescale(a0, a1, b0, b1, x) {
-  return b0 + (x - a0) / (a1 - a0) * (b1 - b0);
-}
-
 function draw() {
   var canvas = document.getElementById('canvas');
   canvas.width = canvas.offsetWidth;
@@ -112,80 +107,50 @@ function draw() {
   ctx.stroke();
 
   var p = [2.7,0.7];
-  var c_i = [4,2], r_i = 0.7;
-  var c_j = [1,3.4], r_j = 0.3;
-  var c_k = [4,4.5], r_k = 0.3;
+  var cs = [[4,2],  [1,3.4], [4,4.5]];
+  var rs = [0.7,    0.3,     0.3];
 
   ctx.beginPath();
   ctx.arc(p[0], p[1], 0.02, 0, 2 * Math.PI);
   ctx.fill();
 
-  ctx.beginPath();
-  ctx.arc(c_i[0], c_i[1], r_i, 0, 2 * Math.PI);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(c_j[0], c_j[1], r_j, 0, 2 * Math.PI);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(c_k[0], c_k[1], r_k, 0, 2 * Math.PI);
-  ctx.stroke();
+  for (var i = 0; i < cs.length; i++) {
+    ctx.beginPath();
+    ctx.arc(cs[i][0], cs[i][1], rs[i], 0, 2 * Math.PI);
+    ctx.stroke();
+  }
 
   var N = 50000;
-  for (var i = 0; i < N; i++) {
-    var alpha = rescale(0, 1, 0, 2 * Math.PI, i / (N-1));
+  for (var k = 0; k < N; k++) {
+    var alpha = 2 * Math.PI * k / (N-1);
     var d = vec.from_polar(alpha, 1);
 
-    var Pi_Di = circ.Pi_Di(p, d, c_i, r_i);
-    var P_i = Pi_Di[0];
-    var D_i = Pi_Di[1];
+    var prev_Pi = p;
+    var prev_Di = d;
+    for (var i = 0; i < cs.length; i++) {
+      var Pi_Di = circ.Pi_Di(prev_Pi, prev_Di, cs[i], rs[i]);
+      var P_i = Pi_Di[0];
+      var D_i = Pi_Di[1];
 
-    ctx.beginPath();
-    ctx.moveTo(p[0], p[1]);
-    ctx.lineTo(P_i[0], P_i[1]);
-    ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(prev_Pi[0], prev_Pi[1]);
+      ctx.lineTo(P_i[0], P_i[1]);
+      ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(P_i[0], P_i[1], 0.02, 0, 2 * Math.PI);
-    ctx.fill();
+      ctx.beginPath();
+      ctx.arc(P_i[0], P_i[1], 0.02, 0, 2 * Math.PI);
+      ctx.fill();
 
-    var q = vec.clone(P_i);
-    vec.add(q, q, D_i);
-    ctx.beginPath();
-    ctx.moveTo(P_i[0], P_i[1]);
-    ctx.lineTo(q[0], q[1]);
-    ctx.stroke();
+      var q = vec.clone(P_i);
+      vec.add(q, q, D_i);
+      ctx.beginPath();
+      ctx.moveTo(P_i[0], P_i[1]);
+      ctx.lineTo(q[0], q[1]);
+      ctx.stroke();
 
-    var Pj_Dj = circ.Pi_Di(P_i, D_i, c_j, r_j);
-    var P_j = Pj_Dj[0];
-    var D_j = Pj_Dj[1];
-
-    ctx.beginPath();
-    ctx.arc(P_j[0], P_j[1], 0.02, 0, 2 * Math.PI);
-    ctx.fill();
-
-    q = vec.clone(P_j);
-    vec.add(q, q, D_j);
-    ctx.beginPath();
-    ctx.moveTo(P_j[0], P_j[1]);
-    ctx.lineTo(q[0], q[1]);
-    ctx.stroke();
-
-    var Pk_Dk = circ.Pi_Di(P_j, D_j, c_k, r_k);
-    var P_k = Pk_Dk[0];
-    var D_k = Pk_Dk[1];
-
-    ctx.beginPath();
-    ctx.arc(P_k[0], P_k[1], 0.02, 0, 2 * Math.PI);
-    ctx.fill();
-
-    q = vec.clone(P_k);
-    vec.add(q, q, D_k);
-    ctx.beginPath();
-    ctx.moveTo(P_k[0], P_k[1]);
-    ctx.lineTo(q[0], q[1]);
-    ctx.stroke();
+      prev_Pi = P_i;
+      prev_Di = D_i;
+    }
   }
 }
 
